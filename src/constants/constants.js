3a846d7cdcebf47b4c999157bffec491a32d51a3
José Renteria <120423283+joserenter1a@users.noreply.github.com>
2023-10-15 18:38:28 -0700

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 export const projects = [
   {
-    title: 'Calorie Tracker Visualizer',
+    title: 'Calorie Tracker Visualizer (2021)',
     description: "Using Python and Numpy, a simple calorie tracker to help visualize progress.",
       image: 'images/1.png',
       tags: ['Python', 'Numpy', 'Matplotlib'],
@@ -9,20 +9,39 @@ export const projects = [
     id: 0,
   },
   {
-    title: 'Pacman Q-Learning Agent',
+    title: 'Pacman Q-Learning Agent (2022)',
     description:"Implementation of an approximate Q-learning agent that learns weights for features of states. Project for CS471 at the University of Oregon. ",
     image: 'images/2.png',
     tags: ['Python3', 'Tkinter'],
     source: 'https://github.com/joserenter1a/CS471-Work/tree/main/reinforcement',
     visit: 'https://raw.githubusercontent.com/joserenter1a/CS471-Work/main/reinforcement/pacman.py',
     id: 1,
-  }
+  },
+    {
+    title: 'TeqPro (2023)',
+    description:"Cloud-Based SaaS Technical Interview Platform with built-in code editor and real-time audio & video conferencing. ",
+    image: 'images/3.png',
+    tags: ['React', 'Node.js', 'Agora-SDK'],
+    source: 'https://github.com/joserenter1a/teq-pro',
+    visit: 'https://www.youtube.com/watch?v=yEGAUOvKUmY',
+    id: 2,
+  },
+  {
+    title: 'DreamJob (2023)',
+    description:"A Cross-Platform mobile application built to search for and land your dream job. Powered by JSearch API",
+    image: 'images/4.png',
+    tags: ['React-Native', 'Node.js', 'Expo-CLI', 'JSearch Api'],
+    source: 'https://github.com/joserenter1a/react-native-dreamjob',
+    visit: 'https://github.com/joserenter1a/react-native-dreamjob',
+    id: 3,
+  },
 ];
 
 export const TimeLineData = [
   { year: 2017, text: 'Graphic Designer for iSportsnaps', },
   { year: 2020, text: 'Began CS Degree at University of Oregon', },
   { year: 2021, text: 'Hired as a CS Encore Leader', },
-  { year: 2023, text: 'Joined UO Software Consultants', },
+  { year: 2023, text: 'Interned at Connected Lane County as Full-Stack Mobile Developer', },
+  { year: 2023, text: 'Granted UO DucksRISE Research Fellowship to conduct Generative AI Research', },
   { year: 2024, text: 'Graduating with Bachelors in Computer Science, with a Minor in Multimedia', },
 ];
