7fefb80f5c64c29edb3d69377007d7c6f6314e40
José Renteria <120423283+joserenter1a@users.noreply.github.com>
2024-10-13 18:51:59 -0700

    Update default.js

--- src/themes/default.js
+++ src/themes/default.js
@@ -6,7 +6,7 @@ export default {
   },
   // Colors for layout
   colors: {
-    primary1: "hsl(204,23.8%,95.9%)",
+    primary1: "#1d232b",
     background1: "#32343b",
     accent1: "hsl(185.0,69.0%,80.0%)",
     button: "hsl(205.1,100%,36.1%)",
